529a0870705d6cf1161a6915197aab1cd0f8c8d5
8Q4H4 <168399483+8q4h4@users.noreply.github.com>
2024-09-12 16:20:36 +0200

    Update UIElements.lua

--- UIElements.lua
+++ UIElements.lua
@@ -20,7 +20,7 @@ return function()
 	--Properties:
 
 	abc.Name = "abc"
-	abc.Parent = game.ReplicatedStorage
+	abc.Parent = game:GetService("ReplicatedStorage")
 	abc.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 	ESPTemplate.Name = "ESPTemplate"
